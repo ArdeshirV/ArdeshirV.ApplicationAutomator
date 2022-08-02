bf17a31c68f0afc588637d3912fe828608868c26
ArdeshirV <ArdeshirV@protonmail.com>
2022-08-02 04:41:13 +0430

    Upload Files

---
+++ MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Application_Automator
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
